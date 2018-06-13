5f1ba49e4031946e54b4cd0d8fde4ba3d2b4f857
codecowboydotio <24376322+codecowboydotio@users.noreply.github.com>
2018-06-14 09:02:01 +1000

    Create tomcat.js

---
+++ tomcat.js
@@ -0,0 +1,22 @@
+module.exports = {
+  tags: ['tomcat'],
+  '@disabled': false,
+
+  'Test scotts web app' : function (client) {
+    client
+      .url('http://fedora:8080/helloworld')
+      .pause(1000);
+
+    client.expect.element('title').to.be.present;
+    client.expect.element('body').to.be.present;
+    client.expect.element('h1').to.be.present;
+
+    client.assert.title('Hello World');
+    client.assert.containsText('body', 'Hello, World!')
+    client.assert.containsText('body', 'Server Address')
+    client.assert.containsText('body', 'Java Version:')
+    client.assert.containsText('body', 'Operating System Name: Linux')
+
+    client.end();
+  }
+};
